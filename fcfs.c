efae926fd76cb3b0d6fcf7d6f116ce7143e5b812
Joshin Joy <joshinjoy22@gmail.com>
2026-03-08 14:17:14 +0530

    FCFS code

--- fcfs.c
+++ fcfs.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int n, i;
-    char pid[20][10];
-    int at[20], bt[20];
-    int wt[20], tat[20];
-    int ct[20];
+    int at[20], bt[20], wt[20], tat[20], ct[20];
+    char pid[20][5];
     float avg_wt = 0, avg_tat = 0;
 
+    printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    for(i = 0; i < n; i++) {
+    printf("Enter PID ArrivalTime BurstTime\n");
+    for(i = 0; i < n; i++)
+    {
         scanf("%s %d %d", pid[i], &at[i], &bt[i]);
     }
 
-    for(i = 0; i < n; i++) {
+    for(i = 0; i < n; i++)
+    {
         if(i == 0)
             ct[i] = at[i] + bt[i];
-        else {
+        else
+        {
             if(ct[i-1] < at[i])
                 ct[i] = at[i] + bt[i];
             else
@@ -26,21 +30,24 @@ int main() {
 
         tat[i] = ct[i] - at[i];
         wt[i] = tat[i] - bt[i];
+    }
 
+    printf("\nWaiting Time:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%s %d\n", pid[i], wt[i]);
         avg_wt += wt[i];
+    }
+
+    printf("\nTurnaround Time:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%s %d\n", pid[i], tat[i]);
         avg_tat += tat[i];
     }
 
-    printf("Waiting Time:\n");
-    for(i = 0; i < n; i++)
-        printf("%s %d\n", pid[i], wt[i]);
-
-    printf("Turnaround Time:\n");
-    for(i = 0; i < n; i++)
-        printf("%s %d\n", pid[i], tat[i]);
-
-    printf("Average Waiting Time: %.2f\n", avg_wt/n);
-    printf("Average Turnaround Time: %.2f\n", avg_tat/n);
+    printf("\nAverage Waiting Time: %.2f", avg_wt/n);
+    printf("\nAverage Turnaround Time: %.2f\n", avg_tat/n);
 
     return 0;
 }
