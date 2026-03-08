54e18e667e03146b72ec602fa269c990086cfa61
Joshin Joy <joshinjoy22@gmail.com>
2026-03-08 14:44:31 +0530

    FCFS program for clarity and correctness

--- fcfs.c
+++ fcfs.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int n,i;
     char pid[10][5];
-    int at[10],bt[10],wt[10],tat[10];
+    int at[10],bt[10];
+    int wt[10],tat[10];
     int time=0;
     float avg_wt=0,avg_tat=0;
 
+    printf("Enter number of processes: ");
     scanf("%d",&n);
 
+    printf("Enter PID ArrivalTime BurstTime\n");
     for(i=0;i<n;i++)
+    {
         scanf("%s %d %d",pid[i],&at[i],&bt[i]);
+    }
 
     for(i=0;i<n;i++)
     {
         if(time < at[i])
-            time = at[i];  
+            time = at[i];   // CPU idle if next process hasn't arrived
 
         wt[i] = time - at[i];
         time = time + bt[i];
@@ -25,16 +31,23 @@ int main() {
         avg_tat += tat[i];
     }
 
-    printf("Waiting Time:\n");
+    printf("\nWaiting Time:\n");
     for(i=0;i<n;i++)
+    {
         printf("%s %d\n",pid[i],wt[i]);
+    }
 
     printf("Turnaround Time:\n");
     for(i=0;i<n;i++)
+    {
         printf("%s %d\n",pid[i],tat[i]);
+    }
 
-    printf("Average Waiting Time: %.2f\n",avg_wt/n);
-    printf("Average Turnaround Time: %.2f\n",avg_tat/n);
+    avg_wt = avg_wt/n;
+    avg_tat = avg_tat/n;
+
+    printf("Average Waiting Time: %.2f\n",avg_wt);
+    printf("Average Turnaround Time: %.2f\n",avg_tat);
 
     return 0;
 }
